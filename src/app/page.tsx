c80d21bbcee92b7a1484c87df22e67db0c042ff2
Abish-x-lethal <magarabish456@gmail.com>
2023-06-01 21:19:20 +0545

    img check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Patan from './../../public/patan.jpg'
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex justify-between h-screen space-x-2'>
+    <div>
+      <Image alt='img' src={Patan} className='w-full h-full object-cover' />
+    </div>
+    <div>
+      <Image alt='img' src={Patan} className='w-full h-full object-cover' />
+    </div>
+    <div>
+      <Image alt='img' src={Patan} className='w-full h-full object-cover' />
+    </div>
+  </div>
   )
 }
 
